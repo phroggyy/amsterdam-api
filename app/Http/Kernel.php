543337ebf5d59c8a1e95986787f19097df891db6
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-03-15 13:45:49 +0100

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends HttpKernel {
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
 		'App\Http\Middleware\VerifyCsrfToken',*/
-		//'App\Http\Middleware\ApiKeyMiddleware',
+		'App\Http\Middleware\ApiKeyMiddleware',
 	];
 
 	/**
