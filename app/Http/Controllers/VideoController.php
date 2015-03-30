5153846c726984f0ac445d98409bb09425b49677
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-03-30 18:17:38 +0200

    new vimeo implementation

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -4,8 +4,31 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use Vimeo\Vimeo;
 
 class VideoController extends Controller {
+
+	private $vimeo;
+
+	/**
+	 * VideoController constructor.
+	 * @param $vimeo
+	 */
+	public function __construct()
+	{
+		$clientId = '14321efccb44b30e24fd45ef7156c40501bdb330';
+		$clientSecret = '0c0e12d6f0d388a355c49ca0e8118544645f2cea';
+		$this->vimeo = new Vimeo($clientId, $clientSecret);
+
+		if (!isset($_SESSION['token'])) {
+			// Set credentials
+			// These should actually be saved in a cookie to not constantly reauth
+			$token = $vimeo->clientCredentials();
+			$_SESSION['token'] = $token;
+		}
+		$this->vimeo->setToken($_SESSION['token']['body']['access_token'])
+	}
+
 
 	/**
 	 * Display a listing of the resource.
@@ -14,8 +37,7 @@ class VideoController extends Controller {
 	 */
 	public function index()
 	{
-		require '../../auth_vimeo.php';
-		$response = $vimeo->request('/users/23320497/videos', [], 'GET');
+		$response = $this->vimeo->request('/users/23320497/videos', [], 'GET');
 		echo json_encode($response['body']['data']);
 	}
 
